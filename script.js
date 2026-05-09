365877339428dc55a3d7c5d9518bae8edc67a866
nezrin <nezrinheseneliyeva380@gmail.com>
2026-05-09 23:19:51 +0400

    Tekrar

---
+++ script.js
@@ -0,0 +1,130 @@
+// let obyekt={
+  
+// ad:"Nezrin",
+// soyad:"Heseneliyeva",
+  
+// }
+// console.log(obyekt)
+
+// let meyveler =["alma","armud", "banan"];
+
+// console.log( "arraydaki deyer sayi:" ,meyveler.length);
+// console.log( "ilk element :", meyveler[0]);
+// console.log("son element :",meyveler [meyveler.length - 1]);
+// meyveler.push("alca")
+// console.log("alca elave olundu:" ,meyveler);
+// meyveler.splice(-2,2 );
+// console.log("son iki deyer silindi:",meyveler);
+
+// let obyekt={
+
+// ad:"Murad",
+// soyad:"Heseneliyev",
+// tehsil:"orta mekteb",
+
+//  obyekt2 : {
+//   ad:"Nezrin",
+//   soyad:"Heseneliyeva",
+//   tehsil:"Univeristet",
+// },
+// bacarıqlar:{
+//   kolasdirma:["html","css","js"]
+// }
+// }
+// console.log("obyekt yaradildi:",obyekt);
+// console.log("obyekt daxilinde obyekt :", obyekt.obyekt2);
+// console.log("obyekt daxilinde array :", obyekt.bacarıqlar);
+
+
+// let istifadeciler=[
+// {
+//   id:"Nezrin",
+//   ixtisas:"frontend"
+// },
+// {
+//   id:"Murad",
+//   ixtisas:"UI/UX"
+// }
+// ]
+// console.log("array icinde obyekt:",istifadeciler);
+
+// array yaratmaq
+
+// let telebeler=["Sevda","Nezrin","Sebine","Aysel"];
+// console.log(telebeler);
+// console.log(telebeler[0]);
+// telebeler.push("Murad");
+// console.log(telebeler);
+
+// // array icinde array
+// let matris=[
+
+// ["Nezrin","Aysel","Fidan"],
+
+// ["Ayla","Bekir","Elnur"]
+  
+// ]
+// console.log(matris);
+// console.log(matris[1][2]);
+// console.log(matris.join(","));
+// console.log(matris[0].join("-"));
+// donguler
+
+// let list=[1,2,3,4,5,6];
+
+// for(i=1; i<list.length; i++){
+//   let kvadratlar= list[i] * list[i]
+//   console.log(list[i] + "-in kvadrati" + " " + kvadratlar)
+
+// }
+// let list=[1,2,3,4,5,6];
+
+// for (let i = 0; i < list.length; i++) {
+
+//   if (list[i] % 2 == 0) {
+//     console.log(list[i] + " cüt rəqəmdir.");
+//   }
+// }
+
+
+// Ededin cut tek olmasini yoxla
+// let new_task=[2,5,8,9,15,24];
+
+// for (let i = 0; i<new_task.length; i++) {
+//   if (new_task[i]%2==0) {
+// console.log('cut ededdir:', new_task[i]);
+
+//   }
+//   else{
+//     console.log('tek ededdir:',new_task[i]);
+    
+//   }
+  
+// }
+// YALNIZ TEK EDEDLERI YAZDIR
+// let two_task=[4,3,17,19,12,13];
+// for (let i = 0; i< two_task.length; i++) {
+//  if (two_task[i]%2!==0) {
+//   console.log('tek ededdir:', two_task[i]);
+  
+//  }
+  
+// }
+
+// TEKRARLARI SILMEK
+// let text_task=['Nezrin','Nezrin','Murad','Mehemmedeli'];
+
+// let new_task=[... new Set(text_task)];
+// console.log( new_task);
+
+
+
+
+
+
+
+
+
+
+
+
