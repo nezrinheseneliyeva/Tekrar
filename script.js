4b37903735045cf83cc3aa7d0bf000e6c958464d
nezrin <nezrinheseneliyeva380@gmail.com>
2026-05-13 22:28:35 +0400

    sliders

--- script.js
+++ script.js
@@ -118,13 +118,177 @@
 // console.log( new_task);
 
 
+// let deyer = document.getElementById("header");
+// let buton = document.getElementById("btn");
+
+// deyer.innerText = "MURAD";
+// console.log(deyer);
+
+// buton.addEventListener("click",function(){
+//     deyer.innerText ="Aysel"
+   
+   
+// })
+
+// let deyer = document.querySelectorAll(".item");
+// let buton = document.getElementById("btn");
+
+// buton.addEventListener("click", function() { 
+    
+//     deyer.forEach(function(tekElement) { 
+//         tekElement.innerText = "Nezrin";
+//         tekElement.style.color = "purple"; 
+//     });
+
+// });
 
 
+// let deyer = document.getElementById("testElement");
+// let buton = document.getElementById("btnClassName");
+
+// buton.addEventListener("click",function () {
+//     deyer.className = "red-bg"
+    
+// });
+// buton.addEventListener("click",function(){
+//     deyer.classList= "red-bg"
+// });
+
+// let elave = document.getElementById('text');
+// let btn_1 =document.getElementById('add_text');
+// let btn_2 =document.getElementById('clear_text');
+// let siyahi =document.getElementById('list');
+
+// btn_1.addEventListener('click',function(){
+
+//     let yeniList=elave.value;
+
+//     if(yeniList.trim() === ""){
+// return;
+//     }
+
+//         if(yeniList.length <5){
+// alert("adi uzadin")
+// return;
+//     }
+// siyahi.innerHTML +=`
+
+// <li>
+// ${yeniList}
+
+// <button onclick=this.parentElement.remove()>X</button>
 
 
+// </li>
+
+// `;
+
+// elave.value=""
+
+// })
+// document.getElementById('clear_text').onclick = function() {
+//     siyahi.innerHTML = "";
+
+//     }
+const slider_photo=document.querySelector('#slider_photo');
+const button_groups=document.querySelector('#button_groups');
+const slider_title=document.querySelector('#slider_title');
+const prive_button=document.querySelector('#prive_button');
+const next_button=document.querySelector('#next_button');
+const sliderText=document.querySelector('#slider-text');
+const sliderDesc=document.querySelector('#slider-desc');
+const photo =document.querySelector('#photo');
+let sliderIndex= 0;
+let sliderInterval = null;
+
+const sliderData =[
+    {
+        url:"https://www.carpro.com/hs-fs/hubfs/2023-Chevrolet-Corvette-Z06-credit-chevrolet.jpeg?width=1020&name=2023-Chevrolet-Corvette-Z06-credit-chevrolet.jpeg",
+        title:"Best-Selling Sports Cars For All Of 2021",
+        text:"20.01.2014"
+    },
+      {
+        url:"https://media.wired.com/photos/63b8d0a771c6b526845f15a6/master/pass/CES-2023-PEUGEOT_INCEPTION_CONCEPT_2301CN202.jpg",
+        title:"The Best Cars, Rides, and Auto Tech of CES 2023 | WIRED",
+        text:"11.07.2014"
+    },
+      {
+        url:"https://robbreport.com/wp-content/uploads/2024/08/temerario01.jpg?w=800",
+        title:"The 50 Most Expensive Cars in the World",
+        text:"22.10.2024"
+    }
+];
+
+const nextSlider =function(){
+
+ if(sliderIndex < sliderData.length -1){
+sliderIndex++;
+ }
+ else{
+  sliderIndex=0
+ }
+
+uiCreate()
+}
 
 
+const prevSlider =function(){
+
+  if (sliderIndex>0) {
+
+     sliderIndex --;
+  }
+  else{
+    sliderIndex = sliderData.length -1
+  }
+ 
+  uiCreate()
+}
 
 
+button_groups.addEventListener('click',function(e){
+
+const element = e.target.parentElement.tagName === 'BUTTON' ? e.target.parentElement :e.target;
+if(element.tagName ==='BUTTON'){
+
+  const navigation = element.getAttribute('data-nav');
+
+  if(navigation === 'prev'){
+prevSlider()
+  }
+  else if(navigation === 'next'){
+nextSlider()
+  }
+}
+
+});
+
+const intervalSlider = function () {
+   sliderInterval = setInterval(function() {
+    nextSlider();
+}, 3000);
+}
 
 
+const uiCreate= function () {
+  const slider= sliderData[sliderIndex];
+  console.log(slider);
+  
+ if(slider){
+photo.src = slider.url
+sliderText.innerText = slider.title
+sliderDesc.innerText =slider.text
+ }
+  
+};
+
+
+button_groups.addEventListener('mouseover',function(){
+  clearInterval( sliderInterval)
+});
+
+button_groups.addEventListener('mouseout',function(){
+intervalSlider();
+});
+
+uiCreate()
